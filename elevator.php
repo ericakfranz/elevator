d9a3ddab8bb850caee1a61fffff8fb307d2062dc
ericakfranz <erica@fatpony.me>
2015-04-23 19:07:09 -0700

    Elevator is a streamlined little WordPress plugin with only one purpose – to soothingly transport your visitors back to the top of the page!
    Based on the brilliant script
    [Elevator.js](https://github.com/tholman/elevator.js). How can you
    resist the familiar notes and smooth ride we've all come to expect on a
    quality elevator ride?
    
    Just activate and enjoy!
    
    **Customize Elevator**
    You can also customize the appearance of Elevator with CSS. Need help?
    See the [Elevator Style
    Guide](https://fatpony.me/plugins/elevator/style-guide/).
    
    Elevator.js created by [Tim Holman](http://tholman.com/).
    Audio (sourced from [BenSound](http://www.bensound.com/)) is Creative
    Commons.

---
+++ elevator.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Plugin Name: Elevator
+ * Description: Painlessly add Jon Holman's elevator.js to your site. A scroll-to-top solution to musically soothing your visitors while being smoothly scrolled to the top of their screen.
+ * Version: 1.0.0
+ * Author: Erica Franz
+ * Author URI: https://fatpony.me/
+ * Text Domain: elevator
+ * License: MIT
+ */
+
+defined( 'ABSPATH' ) or die( 'Please enjoy the music during your wait.' );
+
+// Register Scripts
+function elevator_enqueue_scripts() {
+    
+    $path = plugin_dir_url( __FILE__ );
+    
+    // Scripts   
+	wp_register_script( 'elevator', $path . 'assets/js/elevator.min.js', false, '1.0.0', true );
+	wp_enqueue_script( 'elevator' );
+
+}
+
+// Hook into the 'wp_enqueue_scripts' action
+add_action( 'wp_enqueue_scripts', 'elevator_enqueue_scripts' );
+
+// Add an elevator button
+function elevator_button() {
+    
+        // Begin Elevator Container
+        $button = '<div id="elevator" class="elevator-container">';
+            // Basic Inline Style
+                $button .= '<style type="text/css" scoped="scoped">#elevator{text-align:center;}.elevator-button{padding:20px;width:auto;margin:auto;display:inline-block;}.elevator-button:hover{cursor:pointer;}</style>';
+            // Begin Elevator Button Container
+            $button .= '<div class="elevator-button">';
+                // Button Text
+                $button .= __( 'Back to Top', 'elevator' ) . '</div>';
+            // End Elevator Button Container
+            $button .= '</div>';
+        // End Elevator Container
+        $button .= '</div>';
+    
+    echo $button;
+    
+}
+add_action( 'wp_footer', 'elevator_button', 10 );
+
+// And, elevate!
+function elevator_script() {
+    
+    $path = plugin_dir_url( __FILE__ );
+    
+    $script = '<script type="text/javascript">';
+        $script .= 'window.onload = function() {';
+            $script .= 'var elementButton = document.querySelector(\'.elevator-button\');';
+            $script .= 'var elevator = new Elevator({';
+                $script .= 'element: elementButton,';
+                $script .= 'mainAudio: \'' . $path . 'assets/music/elevator-music.mp3\','; // Music from http://www.bensound.com/
+                $script .= 'endAudio: \'' . $path . 'assets/music/ding.mp3\''; // Music from http://www.bensound.com/
+            $script .= '});';
+        $script .= '}';
+    $script .= '</script>';
+    
+    echo $script;
+    
+}
+add_action( 'wp_footer', 'elevator_script', 20 );
---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== Elevator ===
+Contributors: ericakfranz
+Tags: scrolltotop,backtotop,navigation,music
+Requires at least: 3.5
+Tested up to: 4.2
+Stable tag: trunk
+License: MIT
+License URI: http://opensource.org/licenses/MIT
+
+Elevator is a streamlined little WordPress plugin with only one purpose – to soothingly transport your visitors back to the top of the page!
+
+== Description ==
+Based on the brilliant script [Elevator.js](https://github.com/tholman/elevator.js). How can you resist the familiar notes and smooth ride we've all come to expect on a quality elevator ride? 
+
+Just activate and enjoy!
+
+**Customize Elevator**
+You can also customize the appearance of Elevator with CSS. Need help? See the [Elevator Style Guide](https://fatpony.me/plugins/elevator/style-guide/).
+
+
+Elevator.js created by [Tim Holman](http://tholman.com/).
+Audio (sourced from [BenSound](http://www.bensound.com/)) is Creative Commons.
+
+== Installation ==
+1. Install Elevator either via the WordPress.org plugin repository or by uploading the files to your server.
+2. Activate Elevator.
+3. Enjoy the elevator ride!
+
+== Frequently Asked Questions ==
+
+= No 'Back to Top' Button =
+
+Elevator uses the wp_footer(); tag to hook into. If your theme doesn’t include this tag somewhere (it’s most commonly found in the footer.php template file, just before the closing tag), then Elevator can’t work it’s magic. Check your theme first if Elevator’s ‘Back to Top’ button isn’t appearing on the page after you’ve activated the plugin.
+
+= 'Back to Top' Button Not Working =
+
+If your ‘Back to Top’ button is loading, but the Elevator seems to be broken and stuck in the Basement, check for JavaScript errors in your browser’s console. You can use Google Chrome’s Dev Tools for this. If you’re unfamiliar with checking for JavaScript errors in your browser, I suggest you check out Julie Pagano’s [JavaScript Debugging for Beginners](http://juliepagano.com/blog/2014/05/18/javascript-debugging-for-beginners/#developer-tools) article.
+
+If your site is generating a JavaScript error, it’s possible for it to conflict with Elevator and I suggest you resolve the JavaScript error before proceeding with any other troubleshooting.
+
+== Changelog ==
+= 1.0.0 =
+* Initial Release
